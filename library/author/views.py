d4415e75bbd12776d8322174e7d02141db6a5db5
Vladyslav Bondarenko <pertosel1901@gmail.com>
2026-06-14 14:38:36 +0300

    Added forms for author views and template

--- library/author/views.py
+++ library/author/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Author
+from .forms import AuthorForm
 
 
 
@@ -10,23 +11,28 @@ def authors_list_view(request):
 
 
 def author_create_view(request):
-    # Перевірка прав (тільки адмін/бібліотекар)
+
     if not request.user.is_authenticated or (
             not request.user.is_superuser and getattr(request.user, 'role', None) != 1):
         return redirect('login')
 
     if request.method == 'POST':
-        name = request.POST.get('name')
-        surname = request.POST.get('surname')
-        patronymic = request.POST.get('patronymic')
 
-        if name:
-            Author.objects.create(name=name, surname=surname, patronymic=patronymic)
-        return redirect('authors_list')
+        form = AuthorForm(request.POST)
 
-    return render(request, 'author/author_form.html')
+        if form.is_valid():
+            form.save()
 
+            return redirect('authors_list')
 
+    else:
+        form = AuthorForm()
+
+    return render(
+        request,
+        'author/author_form.html',
+        {'form': form}
+    )
 
 def author_delete_view(request, author_id):
     # Перевірка прав
